ff65fc51045aeda5eda64b5262de87a7f12b4e2c
Daniel T <playdany252@gmail.com>
2019-04-11 11:57:50 -0500

    Add files via upload

---
+++ UTPL/src/EjemploClase23.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class EjemploClase22 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String nombre;
+        String apellido;
+        int edad;
+        String ciudad, pais;
+		int promedio;
+		
+        System.out.println("Por favor ingrese sus nombre");
+        nombre = entrada.next();
+
+        System.out.println("Por favor ingrese su apellido:");
+        apellido = entrada.next();
+
+        System.out.println("Por favor ingrese su edad");
+        edad = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("Por favor ingrese su ciudad:");
+        ciudad = entrada.next();
+
+		System.out.println("Por favor ingrese su pais:");
+        pais = entrada.next();
+	
+		System.out.println("Por favor ingrese su notas:");
+        notas = entrada.nextInt();
+		
+		System.out.println("Por favor ingrese su promedio:");
+        promedio = entrada.nextInt();
+		
+        System.out.printf("Sus nombres y apellidos: %s %s \n", nombre, apellido);
+        System.out.println("Edad: " + 30);
+        System.out.println("Ciudad: " + ciudad);
+		System.out.println("Pais: " + pais);
+		System.out.println("Notas: " + notas);
+		System.out.println("Promedio: " + promedio);
+    }
+}
